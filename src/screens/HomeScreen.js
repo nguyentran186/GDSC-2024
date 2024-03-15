1be72c373b2228e8ba5373ecfb598d9081ea3d67
nguyentran186 <reignoverabus@gmail.com>
2024-03-15 09:45:08 +0700

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,158 @@
+import React, { useRef, useEffect, useState } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { colors } from "../constants/theme";
+import MainHeader from "../components/MainHeader";
+import ScreenHeader from "../components/ScreenHeader";
+import { Pose } from "@mediapipe/pose";
+import { FaceMesh } from "@mediapipe/face_mesh";
+import * as Facemesh from "@mediapipe/face_mesh";
+import * as pose from "@mediapipe/pose";
+import * as cam from "@mediapipe/camera_utils";
+import * as draw from '@mediapipe/drawing_utils'
+import * as control from '@mediapipe/control_utils'
+import * as control3d from '@mediapipe/control_utils_3d'
+import Webcam from "react-webcam";
+import { io } from 'socket.io-client';
+
+const socket = io('http://127.0.0.1:5000');
+
+function HomeScreen() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [landmark, setLandmark] = useState([])
+  const [response, setResponse] = useState("");
+
+  function onResults(results) {
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    // Set canvas width
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(results.image, 0, 0,
+      canvasElement.width, canvasElement.height);
+      
+    if (results.poseLandmarks) {
+      canvasCtx.globalCompositeOperation = 'source-over';
+      draw.drawConnectors(canvasCtx, results.poseLandmarks, pose.POSE_CONNECTIONS,
+                     {color: '#00FF00', lineWidth: 4});
+      draw.drawLandmarks(canvasCtx, results.poseLandmarks,
+                    {color: '#FF0000', lineWidth: 2});
+      canvasCtx.restore();
+
+      setLandmark(results.poseLandmarks)
+    }
+  }
+
+  useEffect(() => {
+    socket.emit('pose_landmarks', landmark);
+  }, [landmark]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    socket.on('response', (data) => {
+      setResponse(data);
+    });
+
+    const pose = new Pose({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+      },
+    });
+    
+    pose.setOptions({
+      modelComplexity: 1,
+      smoothLandmarks: true,
+      enableSegmentation: true,
+      smoothSegmentation: true,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5
+    });
+
+    pose.onResults(onResults);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      const camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await pose.send({ image: webcamRef.current.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start();
+    }
+
+    return () => {
+      socket.disconnect();
+    };
+    
+  }, []);
+
+  return (
+    <center>
+      <div className="App">
+        <div>
+          <p>Response from backend: {response}</p>
+        </div>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          className="output_canvas"
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </div>
+    </center>
+  );
+}
+
+
+export default HomeScreen;
